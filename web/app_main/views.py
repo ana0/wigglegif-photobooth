fc8cb0b3d50a88f80af7391dc7bb48e6627bdb10
ana0 <sarah.anne.friend@gmail.com>
2016-01-02 15:23:14 -0500

    images posting dynamically

--- web/app_main/views.py
+++ web/app_main/views.py
@@ -1,9 +1,15 @@
 from flask import render_template, Flask, redirect, url_for, flash
 from app_main import app
-#from .forms import AddInventoryItemForm, EditInventoryItemForm, DeleteConfirm
-#from .. import db
-#from ..models import Inventory, Material, Design, Piece, designs, pieces, materials
+import os
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	gifs = []
+	for root, dirs, files in os.walk("/media/ana0/000/Documents/wigglegif/giftest"):
+	    for f in files:
+	        if f.endswith('.GIF'):
+	            gifs.append(f)
+    #files = [files for root, dirs, files, in os.walk("/media/ana0/000/Documents/wigglegif/giftest")]
+    #gifs = [f for f in files if f.endswith('.gif')]
+    #gifs = "gifs"
+	return render_template('index.html', gifs=gifs)
